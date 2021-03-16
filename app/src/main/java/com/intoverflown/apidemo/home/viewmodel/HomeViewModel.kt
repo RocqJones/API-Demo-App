96c86ef43ba3e14dc17656e8d9de22f466b64782
RocqJones <jonesmbindyo@gmail.com>
2021-03-16 21:25:05 +0300

    ViewModel

--- app/src/main/java/com/intoverflown/apidemo/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/intoverflown/apidemo/home/viewmodel/HomeViewModel.kt
@@ -1,4 +1,37 @@
 package com.intoverflown.apidemo.home.viewmodel
 
-class HomeViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.intoverflown.apidemo.home.data.HomeRepository
+import com.intoverflown.apidemo.home.data.PostModel
+
+class HomeViewModel(application: Application): AndroidViewModel(application) {
+    private var homeRepository:HomeRepository?=null
+    var postModelListLiveData : LiveData<List<PostModel>>?=null
+    var createPostLiveData:LiveData<PostModel>?=null
+    var deletePostLiveData:LiveData<Boolean>?=null
+
+    init {
+        homeRepository = HomeRepository()
+        postModelListLiveData = MutableLiveData()
+        createPostLiveData = MutableLiveData()
+        deletePostLiveData = MutableLiveData()
+    }
+
+    // call fetch post from Home Repository
+    fun fetchAllPosts(){
+        postModelListLiveData = homeRepository?.fetchAllPosts()
+    }
+
+    // call create post from Home Repository
+    fun createPost(postModel: PostModel){
+        createPostLiveData = homeRepository?.createPost(postModel)
+    }
+
+    // call delete post from Home Repository
+    fun deletePost(id:Int){
+        deletePostLiveData = homeRepository?.deletePost(id)
+    }
 }
