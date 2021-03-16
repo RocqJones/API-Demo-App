e89b3cc7ff0b344b1c76ac88beecc6a9f87a16f4
RocqJones <jonesmbindyo@gmail.com>
2021-03-16 21:25:51 +0300

    Network: Api client and Interface

--- app/src/main/java/com/intoverflown/apidemo/network/ApiInterface.kt
+++ app/src/main/java/com/intoverflown/apidemo/network/ApiInterface.kt
@@ -1,4 +1,16 @@
 package com.intoverflown.apidemo.network
 
+import com.intoverflown.apidemo.home.data.PostModel
+import retrofit2.Call
+import retrofit2.http.*
+
 interface ApiInterface {
+    @GET("posts")
+    fun fetchAllPosts(): Call<List<PostModel>>
+
+    @POST("posts")
+    fun createPost(@Body postModel: PostModel):Call<PostModel>
+
+    @DELETE("posts/{id}")
+    fun deletePost(@Path("id") id:Int):Call<String>
 }
