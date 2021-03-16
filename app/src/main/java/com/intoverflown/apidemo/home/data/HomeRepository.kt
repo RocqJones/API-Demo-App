62da94405f637cbd94467bdbc5dcb7c2de1a8395
RocqJones <jonesmbindyo@gmail.com>
2021-03-16 21:24:32 +0300

    data repository

--- app/src/main/java/com/intoverflown/apidemo/home/data/HomeRepository.kt
+++ app/src/main/java/com/intoverflown/apidemo/home/data/HomeRepository.kt
@@ -1,4 +1,81 @@
 package com.intoverflown.apidemo.home.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.intoverflown.apidemo.network.ApiClient
+import com.intoverflown.apidemo.network.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class HomeRepository {
+
+    private var apiInterface:ApiInterface?=null
+
+    init {
+        apiInterface = ApiClient.getApiClient().create(ApiInterface::class.java)
+    }
+
+    // fetch post
+    fun fetchAllPosts(): LiveData<List<PostModel>> {
+        val data = MutableLiveData<List<PostModel>>()
+
+        apiInterface?.fetchAllPosts()?.enqueue(object : Callback<List<PostModel>> {
+
+            override fun onFailure(call: Call<List<PostModel>>, t: Throwable) {
+                data.value = null
+            }
+
+            override fun onResponse(
+                call: Call<List<PostModel>>,
+                response: Response<List<PostModel>>
+            ) {
+
+                val res = response.body()
+                if (response.code() == 200 &&  res!=null){
+                    data.value = res
+                }else{
+                    data.value = null
+                }
+            }
+        })
+        return data
+    }
+
+    // create new post
+    fun createPost(postModel: PostModel):LiveData<PostModel>{
+        val data = MutableLiveData<PostModel>()
+
+        apiInterface?.createPost(postModel)?.enqueue(object : Callback<PostModel>{
+            override fun onFailure(call: Call<PostModel>, t: Throwable) {
+                data.value = null
+            }
+
+            override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
+                val res = response.body()
+                if (response.code() == 201 && res!=null){
+                    data.value = res
+                }else{
+                    data.value = null
+                }
+            }
+        })
+        return data
+    }
+
+    // Delete post
+    fun deletePost(id:Int):LiveData<Boolean>{
+        val data = MutableLiveData<Boolean>()
+
+        apiInterface?.deletePost(id)?.enqueue(object : Callback<String>{
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                data.value = false
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                data.value = response.code() == 200
+            }
+        })
+        return data
+    }
 }
